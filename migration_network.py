a0c478715d3ffbbdf68a4de6a9f4f38a9f00501a
Shuhan Du <shuhan.du@outlook.com>
2024-05-14 19:45:41 -0700

    calculated average change in democracy index

--- migration_network.py
+++ migration_network.py
@@ -1,15 +1,3 @@
-
-
-'''
-1) open the file for reading
-2) put all lines into an array using readlines()
-3) separate all lines by comma
-4) have a list of region codes you don't want to include
-5) go through the array, if either destination or origin is in the list of 
-regions you don't want to include, exclude the element
-6
-'''
-
 import networkx as nx
 import matplotlib.pyplot as plt
 import sys
@@ -24,49 +12,48 @@ csv_lines = csv_lines[11:]
 split_lines = []
 for line in csv_lines:
     split_lines.append(line.split(','))
-# print(split_lines[0][1]) # Destination
-# print(split_lines[0][3]) # Destination Code
-# print(split_lines[0][5]) # Origin
-# print(split_lines[0][6]) # Origin Code
-# for i in range(7, 14):
-#     print(split_lines[0][i])
-
 
 non_countries = ['900', '947', '1833', '921', '1832', '1830', '1835', '927', '1829', '901', '902', '934', \
                  '948', '941', '1636', '1637', '1503', '1517', '1502', '1501', '1500', '903', '910', '911', \
                     '912', '913', '914', '935', '5500', '906', '920', '5501', '922', '908', '923', '924', \
                         '925', '926', '904', '915', '916', '931', '905', '909', '927', '928', '954', '957']
 countries_list = []
-#print(len(split_lines))
 for line in split_lines:
     if (line[3] not in non_countries and line[6] not in non_countries
         and line[3].isnumeric() and line[6].isnumeric()):
         countries_list.append(line)
-# print(len(countries_list))
-# print('Destination: ' + countries_list[0][1].strip() + ' ' + countries_list[0][3])
-# print('Origin: ' + countries_list[0][5] + ' ' + countries_list[0][6])
-# print('Number of Migrants 1990: ' + countries_list[0][7])
-# Index 1 is destination name, index 3 is destination code
-# Index 5 is origin name, index 6 is origin code
-# Indices 7-13 are the years from 1990-2020 in increments of 5 years
 
-G = nx.DiGraph()
+# get politics data
+f_politics = open("Economist_Democracy_Index_CSV.csv", "r")
+csv_lines = f_politics.readlines()
+csv_lines = csv_lines[1:]
+politics = []
+for line in csv_lines:
+    politics.append(line.split(','))
+
+# get year
 year = sys.argv[1]
+politics_index = 3
 if (year == "1990"):
-    year_index = 7 # 1990
+    year_index = 7
 elif (year == "1995"):
-    year_index = 8 # 1995
+    year_index = 8
 elif (year == "2000"):
-    year_index = 9 # 2000
+    year_index = 9
 elif (year == "2005"):
-    year_index = 10 # 2005
+    year_index = 10
 elif (year == "2010"):
-    year_index = 11 # 2010
+    year_index = 11
+    politics_index = 3
 elif (year == "2015"):
-    year_index = 12 # 2015
+    year_index = 12
+    politics_index = 2
 elif (year == "2020"):
-    year_index = 13 # 2020
+    year_index = 13
+    politics_index = 1
 
+# create graph
+G = nx.DiGraph()
 for line in countries_list:
     dest_name = line[1].strip()
     dest_code = line[3].strip()
@@ -75,15 +62,10 @@ for line in countries_list:
     string_migrants = line[year_index].strip().replace(" ", "")
     if string_migrants != '..':
         num_migrants = int(string_migrants)
-
         G.add_node(dest_code, name=dest_name)
         G.add_node(origin_code, name=origin_name)
-
         if (num_migrants > MIN_EDGE_WEIGHT):
             G.add_edge(origin_code, dest_code, weight=num_migrants)
-
-
-
 
 # BETWEENNESS CENTRALITY
 betweenness = nx.betweenness_centrality(G)
@@ -92,33 +74,25 @@ with open("bc_out.txt", "w") as file:
     for node, bc in sorted_betweenness:
         file.write(f"{G.nodes[node]}: {bc}\n")
 
-
 # CLUSTERING COEFFICIENT
 clustering_directed = nx.clustering(G, weight='weight')
 sorted_clustering = sorted(clustering_directed.items(), key=lambda item: item[1], reverse=True)
-
 with open("cc_out.txt", "w") as file:
     for node, clustering in sorted_clustering:
         file.write(f"{G.nodes[node]}: {clustering}\n")
 
 # Compute the average clustering coefficient across the entire graph
 average_clustering_directed = nx.average_clustering(G, weight='weight')
-
-#print("Directed Clustering Coefficient per Node:")
-#print(clustering_directed)
 print("\nAverage Directed Clustering Coefficient:")
 print(average_clustering_directed)
 
 # DEGREE DISTRIBUTION (in, out, total)
-
 in_degrees = dict(G.in_degree())  # Returns a dictionary with node:in-degree pairs
 out_degrees = dict(G.out_degree())  # Returns a dictionary with node:out-degree pairs
 total_degrees = dict(G.degree())  # Returns a dictionary with node:total-degree (in + out) pairs
-
 in_degree_values = list(in_degrees.values())
 out_degree_values = list(out_degrees.values())
 total_degree_values = list(total_degrees.values())
-
 
 # Set up the plot with subplots
 plt.figure(figsize=(14, 6))  # Wider figure to accommodate two subplots
@@ -139,10 +113,11 @@ plt.xlabel('Out-Degree')
 plt.ylabel('Frequency')
 plt.grid(True, which='both', linestyle='--', linewidth=0.5)
 
+# Show plot
 plt.tight_layout()  # Adjusts subplots to fit into figure area.
 plt.show()
 
-
+# Get latitudes and longitudes
 countries = {}
 with open("country_list.txt", "r") as file:
     for line in file:
@@ -152,12 +127,13 @@ with open("country_list.txt", "r") as file:
         lon = float(parts[2])
         countries[country_name] = (lat, lon)
 
+# Draw map
 plt.figure(figsize=(14, 8))  # Set the size of the map
 m = Basemap(projection='merc', llcrnrlat=-60, urcrnrlat=70, llcrnrlon=-180, urcrnrlon=180, lat_ts=20, resolution='c')
 m.drawcoastlines()
 m.drawcountries()
 
-
+# Remove certain nodes
 pos = {}
 removed_nodes = []
 for node in G.nodes(data=True):
@@ -168,8 +144,6 @@ for node in G.nodes(data=True):
     else:
         print(f"Warning: No coordinates found for {country}")
         removed_nodes.append(node[0])
-
-
 for node in removed_nodes:
     G.remove_node(node)
 
@@ -180,7 +154,7 @@ nx.draw_networkx_nodes(G, pos, node_size=20, node_color='blue', alpha=0.6, ax=pl
 edge_widths = [0.0000005 * G[u][v]['weight'] for u, v in G.edges()]  # Scale down edge widths if they are too large
 nx.draw_networkx_edges(G, pos, width=edge_widths, alpha=0.5, edge_color='black')
 
-
+# Show map
 plt.title('Global Migration Network')
 plt.show()
 
@@ -201,3 +175,33 @@ plt.axis('off')  # Turn off the axis
 plt.title('Network of Migration (' + sys.argv[1] + ')')
 plt.show()
 
+# Get average change in democracy
+total_migrants = 0
+total_change = 0
+for line in countries_list:
+    dest_name = line[1].strip()
+    #print(dest_name)
+    dest_code = line[3].strip()
+    origin_name = line[5].strip()
+    #print(origin_name)
+    origin_code = line[6].strip()
+    string_migrants = line[year_index].strip().replace(" ", "")
+    if string_migrants != '..':
+        num_migrants = int(string_migrants)
+
+        # find the difference in democracy index between the two countries
+        origin_dem = -1
+        dest_dem = -1
+        for country in politics:
+            #print(country[0])
+            if country[0].strip() == origin_name:
+                origin_dem = float(country[politics_index].strip())
+            elif country[0].strip() == dest_name:
+                dest_dem = float(country[politics_index].strip())
+        
+        if (origin_dem != -1 and dest_dem != -1):
+            total_migrants += num_migrants
+            total_change += num_migrants * (dest_dem - origin_dem)
+            #print(str(total_migrants) + " " + str(total_change))
+average_change = total_change / total_migrants
+print("Average change in democracy index: " + str(average_change))
