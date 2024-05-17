31ec7484edb28c8710acac728e42ca2c1bbef1e9
Shuhan Du <shuhan.du@outlook.com>
2024-05-16 21:21:43 -0700

    implemented dem index by population, refactored code more

--- migration_network.py
+++ migration_network.py
@@ -9,30 +9,48 @@ MIN_EDGE_WEIGHT = 100000
 BIN_SIZE = 5
 
 def main():
+    # get data from main UN dataset
+    migration_flows = []
     f = open("undesa_pd_2020_ims_stock_by_sex_destination_and_origin.csv", "r")
     csv_lines = f.readlines()
     csv_lines = csv_lines[11:]
     split_lines = []
     for line in csv_lines:
         split_lines.append(line.split(','))
-
     non_countries = ['900', '947', '1833', '921', '1832', '1830', '1835', '927', '1829', '901', '902', '934', \
                     '948', '941', '1636', '1637', '1503', '1517', '1502', '1501', '1500', '903', '910', '911', \
                         '912', '913', '914', '935', '5500', '906', '920', '5501', '922', '908', '923', '924', \
                             '925', '926', '904', '915', '916', '931', '905', '909', '927', '928', '954', '957']
-    countries_list = []
     for line in split_lines:
         if (line[3] not in non_countries and line[6] not in non_countries
             and line[3].isnumeric() and line[6].isnumeric()):
-            countries_list.append(line)
+            migration_flows.append(line)
+    f.close()
+
+    country_list = []
+    f_countries = open("country_list.txt", "r")
+    csv_lines = f_countries.readlines()
+    for line in csv_lines:
+        country_list.append(line.strip().split(","))
+    f_countries.close()
+
+    # get data from politics dataset
+    politics = []
+    f_politics = open("Economist_Democracy_Index_CSV.csv", "r")
+    csv_lines = f_politics.readlines()
+    csv_lines = csv_lines[1:]
+    for line in csv_lines:
+        politics.append(line.split(','))
+    f_politics.close()
 
     # get year and politics indices
     year_index = get_year_index()
+    population_index = get_population_index()
     politics_index = get_politics_index()
 
     # create graph and get data
     G = nx.DiGraph()
-    data = get_data(G, countries_list, year_index, politics_index)
+    data = get_data(G, migration_flows, country_list, politics, year_index, population_index, politics_index)
     
     # BETWEENNESS CENTRALITY
     betweenness_centrality(G)
@@ -49,62 +67,11 @@ def main():
     degree_distribution(G)
 
     # Get latitudes and longitudes
-    draw_map(G)
+    draw_map(G, country_list)
 
-    # Choose a layout that provides better spacing, and adjust parameters if necessary
-    pos = nx.spring_layout(G, scale=20)  # You can increase the scale to spread out nodes
-    plt.figure(figsize=(15, 15))  # Large figure size to accommodate the graph
-    # Nodes
-    node_size = max(100, 7000 / len(G.nodes()))  # Avoid too large node sizes for large graphs
-    nx.draw_networkx_nodes(G, pos, node_size=node_size, node_color='skyblue', alpha=0.6)
-    # Edges
-    edge_widths = [0.0000005 * G[u][v]['weight'] for u, v in G.edges()]  # Scale down edge widths if they are too large
-    nx.draw_networkx_edges(G, pos, width=edge_widths, alpha=0.5, edge_color='black')
-    # Labels
-    font_size = max(8, 100 / len(G.nodes())**0.5)  # Smaller font size for larger graphs
-    nx.draw_networkx_labels(G, pos, font_size=font_size, font_family='sans-serif')
+    draw_network(G)
 
-    plt.axis('off')  # Turn off the axis
-    plt.title('Network of Migration (' + sys.argv[1] + ')')
-    plt.show()
-
-    # Get average change in democracy
-    total_migrants = 0
-    total_change = 0
-    emigrated_dem_index = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    immigrated_dem_index = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    for element in data:        
-        if (element["origin_dem"] != -1 and element["dest_dem"] != -1):
-            total_migrants += element["migrants"]
-            total_change += element["migrants"] * (element["dest_dem"] - element["origin_dem"])
-            immigrated_dem_index[int(element["dest_dem"])] += total_migrants
-            emigrated_dem_index[int(element["origin_dem"])] += total_migrants
-    average_change = total_change / total_migrants
-    print("Average change in democracy index: " + str(average_change))
-
-    # "Is the a specific level of democracy people tend to migrate from or to?"
-    # Set up the plot with subplots
-    plt.figure(figsize=(14, 6))  # Wider figure to accommodate two subplots
-
-    # Plot in-degree distribution
-    plt.subplot(1, 2, 1)  # 1 row, 2 columns, 1st subplot
-    plt.bar([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], emigrated_dem_index, color='green', alpha=0.7, edgecolor='black')
-    plt.title('Democracy Index of countries emigrated from')
-    plt.xlabel('Democracy Index')
-    plt.ylabel('Emigrants')
-    plt.grid(True, which='both', linestyle='--', linewidth=0.5)
-
-    # Plot out-degree distribution
-    plt.subplot(1, 2, 2)  # 1 row, 2 columns, 2nd subplot
-    plt.bar([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], immigrated_dem_index, color='red', alpha=0.7, edgecolor='black')
-    plt.title('Democracy Index of countries immigrated to')
-    plt.xlabel('Democracy Index')
-    plt.ylabel('Immigrants')
-    plt.grid(True, which='both', linestyle='--', linewidth=0.5)
-
-    # Show plot
-    plt.tight_layout()  # Adjusts subplots to fit into figure area.
-    plt.show()
+    do_politics(data, country_list, politics, population_index, politics_index)
 
 def get_year_index():
     year = sys.argv[1]
@@ -123,6 +90,19 @@ def get_year_index():
     elif (year == "2020"):
         return 13
     
+def get_population_index():
+    year = sys.argv[1]
+    if (year == "2020"):
+        return 3
+    elif (year == "2015"):
+        return 4
+    elif (year == "2010"):
+        return 5
+    elif (year == "2000"):
+        return 6
+    elif (year == "1990"):
+        return 7
+
 def get_politics_index():
     year = sys.argv[1]
     if (year == "2020"):
@@ -134,16 +114,9 @@ def get_politics_index():
     else:
         return -1
     
-def get_data(G, countries_list, year_index, politics_index):
-    politics = []
-    f_politics = open("Economist_Democracy_Index_CSV.csv", "r")
-    csv_lines = f_politics.readlines()
-    csv_lines = csv_lines[1:]
-    for line in csv_lines:
-        politics.append(line.split(','))
-
+def get_data(G, migration_flows, country_list, politics, year_index, population_index, politics_index):
     data = [] # dataset containing all migration flows
-    for line in countries_list:
+    for line in migration_flows:
         origin_name = line[5].strip()
         origin_code = line[6].strip()
         dest_name = line[1].strip()
@@ -159,10 +132,19 @@ def get_data(G, countries_list, year_index, politics_index):
                         origin_dem = float(country[politics_index].strip())
                     elif country[0].strip() == dest_name:
                         dest_dem = float(country[politics_index].strip())
-            data.append({"dest_name": dest_name,
-                        "dest_code": dest_code,
-                        "origin_name": origin_name,
+            origin_pop = 0
+            dest_pop = 0
+            for country in country_list:
+                if (country[0].strip() == origin_name):
+                    origin_pop = country[population_index]
+                elif (country[0].strip() == dest_name):
+                    dest_pop = country[population_index]
+            data.append({"origin_name": origin_name,
                         "origin_code": origin_code,
+                        "dest_name": dest_name,
+                        "dest_code": dest_code,
+                        "origin_pop": origin_pop,
+                        "dest_pop": dest_pop,
                         "migrants": num_migrants,
                         "origin_dem": origin_dem,
                         "dest_dem": dest_dem})
@@ -178,6 +160,7 @@ def betweenness_centrality(G):
     with open("bc_out.txt", "w") as file:
         for node, bc in sorted_betweenness:
             file.write(f"{G.nodes[node]}: {bc}\n")
+    file.close()
 
 def clustering_coefficient(G):
     clustering_directed = nx.clustering(G, weight='weight')
@@ -185,6 +168,7 @@ def clustering_coefficient(G):
     with open("cc_out.txt", "w") as file:
         for node, clustering in sorted_clustering:
             file.write(f"{G.nodes[node]}: {clustering}\n")
+    file.close()
 
 def degree_distribution(G):
     in_degrees = dict(G.in_degree())  # Returns a dictionary with node:in-degree pairs
@@ -217,15 +201,13 @@ def degree_distribution(G):
     plt.tight_layout()  # Adjusts subplots to fit into figure area.
     plt.show()
 
-def draw_map(G):
+def draw_map(G, country_list):
     countries = {}
-    with open("country_list.txt", "r") as file:
-        for line in file:
-            parts = line.strip().split(",")
-            country_name = parts[0]
-            lat = float(parts[1])
-            lon = float(parts[2])
-            countries[country_name] = (lat, lon)
+    for country in country_list:
+        country_name = country[0]
+        lat = float(country[1])
+        lon = float(country[2])
+        countries[country_name] = (lat, lon)
     
     # Draw map
     plt.figure(figsize=(14, 8))  # Set the size of the map
@@ -270,6 +252,76 @@ def draw_map(G):
 
     # Show map
     plt.title('Global Migration Network')
+    plt.show()
+
+def draw_network(G):
+    # Choose a layout that provides better spacing, and adjust parameters if necessary
+    pos = nx.spring_layout(G, scale=20)  # You can increase the scale to spread out nodes
+    plt.figure(figsize=(15, 15))  # Large figure size to accommodate the graph
+    # Nodes
+    node_size = max(100, 7000 / len(G.nodes()))  # Avoid too large node sizes for large graphs
+    nx.draw_networkx_nodes(G, pos, node_size=node_size, node_color='skyblue', alpha=0.6)
+    # Edges
+    edge_widths = [0.0000005 * G[u][v]['weight'] for u, v in G.edges()]  # Scale down edge widths if they are too large
+    nx.draw_networkx_edges(G, pos, width=edge_widths, alpha=0.5, edge_color='black')
+    # Labels
+    font_size = max(8, 100 / len(G.nodes())**0.5)  # Smaller font size for larger graphs
+    nx.draw_networkx_labels(G, pos, font_size=font_size, font_family='sans-serif')
+
+    plt.axis('off')  # Turn off the axis
+    plt.title('Network of Migration (' + sys.argv[1] + ')')
+    plt.show()
+
+def do_politics(data, country_list, politics, population_index, politics_index):
+    # calculate population in each "democracy level"
+    populations = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for country in country_list:
+        for country_p in politics:
+            if (country_p[0] == country[0]):
+                populations[int(float(country_p[politics_index]))] += int(country[population_index])
+    print(populations)
+
+    # Get average change in democracy
+    total_migrants = 0
+    total_change = 0
+    emigrated_dem_index = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    immigrated_dem_index = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for element in data:        
+        if (element["origin_dem"] != -1 and element["dest_dem"] != -1):
+            total_migrants += element["migrants"]
+            total_change += element["migrants"] * (element["dest_dem"] - element["origin_dem"])
+            immigrated_dem_index[int(element["dest_dem"])] += element["migrants"]
+            emigrated_dem_index[int(element["origin_dem"])] += element["migrants"]
+    average_change = total_change / total_migrants
+    print("Average change in democracy index: " + str(average_change))
+
+    for i in range(0, len(emigrated_dem_index)):
+        if (populations[i] >= 1):
+            emigrated_dem_index[i] /= populations[i]
+            immigrated_dem_index[i] /= populations[i]
+
+    # "Is the a specific level of democracy people tend to migrate from or to?"
+    # Set up the plot with subplots
+    plt.figure(figsize=(14, 6))  # Wider figure to accommodate two subplots
+
+    # Plot in-degree distribution
+    plt.subplot(1, 2, 1)  # 1 row, 2 columns, 1st subplot
+    plt.bar([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], emigrated_dem_index, color='green', alpha=0.7, edgecolor='black')
+    plt.title('Democracy Index of countries emigrated from')
+    plt.xlabel('Democracy Index')
+    plt.ylabel('Percentage of World Population')
+    plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+
+    # Plot out-degree distribution
+    plt.subplot(1, 2, 2)  # 1 row, 2 columns, 2nd subplot
+    plt.bar([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], immigrated_dem_index, color='red', alpha=0.7, edgecolor='black')
+    plt.title('Democracy Index of countries immigrated to')
+    plt.xlabel('Democracy Index')
+    plt.ylabel('Percentage of World Population')
+    plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+
+    # Show plot
+    plt.tight_layout()  # Adjusts subplots to fit into figure area.
     plt.show()
 
 if __name__ == '__main__':
